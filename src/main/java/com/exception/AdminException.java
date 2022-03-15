547388e8bff1b1a81567713192189c7b8c92f9be
Sai kumar <daripelly.sai-kumar@capgemini.com>
2022-03-15 19:01:43 +0530

    First commit

---
+++ src/main/java/com/exception/AdminException.java
@@ -0,0 +1,10 @@
+package com.exception;
+
+public class AdminException extends Exception{
+	
+	public AdminException(String msg) {
+		super(msg);
+		}
+
+	
+}
---
+++ src/test/java/com/OnlineVegetableSalesProjectApplicationTests.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineVegetableSalesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
+
+
